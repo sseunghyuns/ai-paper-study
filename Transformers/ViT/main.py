f4d5b7033f0733b412ecd26222f94233181f9973
sseunghyuns <allindata96@gmail.com>
2022-06-22 20:52:33 +0900

    Update ViT model implementation

--- Transformers/ViT/main.py
+++ Transformers/ViT/main.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from tqdm import tqdm
 
 import argparse
+from model import ViT
 from input_patch import PatchDataset
 from eval_functions import evaluation
 
@@ -15,10 +16,9 @@ def parse_args():
     parser.add_argument('--save_acc', default=50, type=int, help='val acc') # acc 50 이상만 저장
     parser.add_argument('--epochs', default=501, type=int, help='training epoch')
     parser.add_argument('--lr', default=2e-3, type=float, help='learning rate')
-    parser.add_argument('--drop_rate', default=0.1, type=float, help='drop rate')
     parser.add_argument('--weight_decay', default=0, type=float, help='weight decay')
     parser.add_argument('--num_classes', default=10, type=int, help='number of classes')
-    parser.add_argument('--latent_dim', default=128, type=int, help='latent dimension') # D
+    parser.add_argument('--latent_vector_size', default=128, type=int, help='latent vector size') # D
     parser.add_argument('--num_heads', default=8, type=int, help='number of heads') # attention head
     parser.add_argument('--num_layers', default=12, type=int, help='number of layers in transformer')
     parser.add_argument('--dataname', default='cifar10', type=str, help='data name')
@@ -32,8 +32,9 @@ def parse_args():
 
 def run(args):
     latent_dim = args.latent_dim
-    mlp_hidden_dim = int(latent_dim/2) # Encoder 내 MLP의 hidden layer 노드 수 
+    hidden_dim = int(latent_dim/2) # Encoder 내 MLP의 hidden layer 노드 수 
     num_patches = int((args.img_size * args.img_size) / (args.patch_size * args.patch_size)) # HW/P^2
+    patch_vec_size = ((args.patch_size**2)*3) # P^2 * C
     device = args.device
     
     if not os.path.exists('./saved'):
@@ -45,64 +46,76 @@ def run(args):
                            batch_size = args.batch_size)
     
     train_loader, valid_loader, test_loader = dataset.load_dataset()
-    
+
     # Load ViT 
-    model = None
+    model = ViT(patch_vec_size=patch_vec_size,
+                num_patches=num_patches,
+                latent_vector_size=latent_dim,
+                num_heads=args.num_heads,
+                hidden_dim=hidden_dim,
+                num_layers=args.num_layers,
+                num_classes=args.num_classes)
     
-    best_accuracy = args.save_acc
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-    
-    for epoch in range(args.epochs):
-        running_loss, num_correct, num_samples = 0, 0, 0
+    if args.mode == 'train' :
+        best_accuracy = args.save_acc
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
         
-        model.train()
-        for i, (X, y) in enumerate(tqdm(train_loader, total=len(train_loader))):
-            X = X.to(device)
-            y = y.to(device)
+        for epoch in range(args.epochs):
+            running_loss, num_correct, num_samples = 0, 0, 0
             
-            outputs, _ = model(X)
-            loss = criterion(outputs, y)
-    
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            
-            preds = torch.argmax(outputs, dim=-1)
-            num_correct += sum(preds==y).sum()
-            num_samples += preds.shape[0]
-            running_loss += loss.item()
-
-        train_accuracy =  num_correct/num_samples
-        train_loss = running_loss/len(train_loader)
+            model.train()
+            for i, (X, y) in enumerate(tqdm(train_loader, total=len(train_loader))):
+                X = X.to(device)
+                y = y.to(device)
+                
+                outputs, _ = model(X)
+                loss = criterion(outputs, y)
         
-        # Evaluation
-        with torch.no_grad():
-            model.eval()
-            valid_accuracy, valid_loss = evaluation(model, valid_loader, criterion, device)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                
+                
+                preds = torch.argmax(outputs, dim=-1)
+                num_correct += sum(preds==y).sum()
+                num_samples += preds.shape[0]
+                running_loss += loss.item()
+
+            train_accuracy =  num_correct/num_samples
+            train_loss = running_loss/len(train_loader)
             
+            # Evaluation
+            with torch.no_grad():
+                model.eval()
+                valid_accuracy, valid_loss = evaluation(model, valid_loader, criterion, device)
+                
 
-    print("Epoch: {}/{}.. ".format(epoch, 50) +
-                "Training Accuracy: {:.4f}.. ".format(train_accuracy) +
-                "Training Loss: {:.4f}.. ".format(train_loss) +
-                "Valid Accuracy: {:.4f}.. ".format(valid_accuracy) + 
-                "Valid Loss: {:.4f}.. ".format(valid_loss))
+        print("Epoch: {}/{}.. ".format(epoch, 50) +
+                    "Training Accuracy: {:.4f}.. ".format(train_accuracy) +
+                    "Training Loss: {:.4f}.. ".format(train_loss) +
+                    "Valid Accuracy: {:.4f}.. ".format(valid_accuracy) + 
+                    "Valid Loss: {:.4f}.. ".format(valid_loss))
 
-    if valid_accuracy > best_accuracy:
-        print("Valid Acc improved from {:.4f} -> {:.4f}".format(best_accuracy, valid_accuracy))
-        best_accuracy = valid_accuracy
+        if valid_accuracy > best_accuracy:
+            print("Valid Acc improved from {:.4f} -> {:.4f}".format(best_accuracy, valid_accuracy))
+            best_accuracy = valid_accuracy
+            
+            # 기존 경로 제거
+            try:
+                os.remove(save_path)
+            except:
+                pass
+            save_path = "saved/epoch{}_.tar".format(epoch+1)
         
-        # 기존 경로 제거
-        try:
-            os.remove(save_path)
-        except:
-            pass
-        save_path = "saved/epoch{}_.tar".format(epoch+1)
-    
-        torch.save(model.state_dict(), save_path)
-
-    
+            torch.save(model.state_dict(), save_path)
+    else:
+        model.eval()
+        test_accuracy, test_loss = evaluation(model, test_loader, criterion, device)
+        print("Test Accuracy: {:.4f}.. ".format(test_accuracy) +
+                "Test Loss: {:.4f}.. ".format(test_loss))
+               
+            
 if __name__ == '__main__':
     args = parse_args()
     # run(args)
--- Transformers/ViT/model.py
+++ Transformers/ViT/model.py
@@ -1,1 +1,160 @@
 # ViT model implementation
+
+import torch
+import torch.nn as nn
+
+# Linear Projection of Flattened Patches
+# (1 x P^2C)크기의 하나의 이미지 패치를 (1 x D)로 임베딩
+class LinearProjection(nn.Module):
+    
+    def __init__(self, patch_vec_size, num_patches, latent_vector_size):
+        super().__init__()
+        self.linear_projection = nn.Linear(patch_vec_size, latent_vector_size)
+        self.class_token = nn.Parameter(torch.randn(1, latent_vector_size)) # 1xD 크기의 학습 가능한 파라미터를 concat해야 한다. 
+        self.positional_embedding = nn.Parameter(torch.randn(1, num_patches+1, latent_vector_size)) # n_patches+class_token
+        self.dropout = nn.Dropout(0.1)
+        
+    def forward(self, x):
+        # input x의 크기: [Batch size x num_patches x (C x P x P)]
+        batch_size = x.size(0)
+        cls_token = self.class_token.repeat(batch_size, 1, 1) # [1xD] -> [B x 1 x D]
+        
+        x = self.linear_projection(x) # [B x num_patches x (C x P^2)] -> [B x num_patches x D]
+        x = torch.cat([cls_token, x], dim=1) # [B x num_patches x D] -> [B x num_patches+1 x D]
+        x += self.positional_embedding
+        x = self.dropout(x)
+        
+        return x
+        
+        
+# Multi-Head self Attention
+class MSA(nn.Module):
+    def __init__(self, latent_vector_size, num_heads):
+        super().__init__()
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.num_heads = num_heads
+        self.latent_vector_size = latent_vector_size
+        self.head_dim = int(latent_vector_size / num_heads) # D_h = (D/k)
+        
+        self.Q = nn.Linear(latent_vector_size, latent_vector_size) # [D] -> [k*D_h(D_h=D/k)] : Multi head가 한번에 계산된다는 의미
+        self.K = nn.Linear(latent_vector_size, latent_vector_size)
+        self.V = nn.Linear(latent_vector_size, latent_vector_size)
+        
+        self.scale = torch.sqrt(latent_vector_size*torch.ones(1)).to(device) # 학습 파라미터가 아니기 때문에, cpu로 존재. gpu로 올려주자.
+        self.dropout = nn.Dropout(0.1)
+        
+    def forward(self, x):
+        batch_size = x.size(0)
+        
+        q = self.Q(x) # B x num_patches+1 x D
+        k = self.K(x)
+        v = self.V(x)
+        
+        # 원래는 [B x num_patches+1 x head_dim x num_heads] 의 4차원 tensor를 계산해야하는데, 3차원으로 계산하고 나중에 4차원으로 쪼개주자.
+        q = q.view(batch_size, -1, self.num_heads, self.head_dim).permute(0,2,1,3) # [B x num_heads x num_patches+1 x head_dim]
+        k = k.view(batch_size, -1, self.num_heads, self.head_dim).permute(0,2,3,1) # q와 내적을 위해 Transpose해준다.
+        v = v.view(batch_size, -1, self.num_heads, self.head_dim).permute(0,2,1,3)
+        
+#         print('q: ', q.shape)
+#         print('k: ', k.shape)
+#         print('v: ', v.shape)
+        
+        qk = torch.matmul(q, k) / self.scale
+        
+        A = torch.softmax(qk, dim=-1) # A=attention
+        A = self.dropout(A)
+        
+        x = torch.matmul(A, v) # [B x num_heads x num_patches+1 x head_dim]
+        x = x.permute(0,2,1,3).reshape(batch_size, -1, self.latent_vector_size) # [B x num_patches+1 x D] (Input과 동일한 크기로 맞춰준다)
+        
+        return x, A
+        
+        
+class TransformerEncoder(nn.Module):
+    '''
+    The Transformer encoder consists of alternating layers of multiheaded self-attention and MLP blocks. 
+    Layernorm is applied before every block, and residual connections after every block.
+    '''
+    def __init__(self, latent_vector_size, num_heads, hidden_dim):
+        super().__init__()
+        
+        self.ln1 = nn.LayerNorm(latent_vector_size)
+        self.ln2 = nn.LayerNorm(latent_vector_size)
+        self.msa = MSA(latent_vector_size, num_heads) # Multi-Head self Attention
+        self.dropout = nn.Dropout(0.1)
+        
+        # The MLP contains two layers with a GELU non-linearity.
+        self.mlp = nn.Sequential(
+            nn.Linear(latent_vector_size, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_dim, latent_vector_size), # [B x num_patches+1 x D]
+            nn.Dropout(0.1)
+        )
+    
+    def forward(self, x):
+        
+        # x: Embedded Patches
+        z = self.ln1(x) # 1. Layer Normalization
+        z, attention = self.msa(z) # 2. Multi-Head Attention
+        z = self.dropout(z) 
+        z_p = z + x # 3. Residual Connection
+        
+        z = self.ln2(z_p) # 4. Layer Normalization
+        z = self.mlp(z) # 5. MLP
+        z += z_p # 5. Layer Normalization
+        
+        return z, attention
+    
+    
+# ViT
+class ViT(nn.Module):
+    def __init__(self, patch_vec_size, num_patches, latent_vector_size, num_heads, hidden_dim, num_layers, num_classes):
+        super().__init__()
+        self.linear_projection = LinearProjection(patch_vec_size, num_patches, latent_vector_size)
+        
+        self.transformer = nn.ModuleList()
+        
+        for _ in range(num_layers):
+            self.transformer.append(TransformerEncoder(latent_vector_size, num_heads, hidden_dim))
+        
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(latent_vector_size),
+            nn.Linear(latent_vector_size, num_classes)
+        )
+        
+    def forward(self, x):
+        att_list = []
+        x = self.linear_projection(x)
+        for encoder in self.transformer:
+            x, att = encoder(x)
+            att_list.append(att)
+            
+        # [B x num_patches+1 x D] -> [B x D](가장 앞에 있는 class token만 떼어온다.)
+        x = self.mlp_head(x[:, 0]) # Only class token
+        
+        return x, att  # [B x Num classes]
+        
+
+if __name__ == '__main__':
+    
+    # settings
+    img_size = 32
+    patch_size = 4
+
+    num_patches = int((img_size/patch_size)**2)
+    latent_vector_size = 128
+    patch_vec_size = patch_size**2 * 3
+    num_heads = 8
+    hidden_dim = 128
+    num_layers = 12
+    num_classes = 10
+    
+    # ViT
+    model = ViT(patch_vec_size, num_patches, latent_vector_size, num_heads, hidden_dim, num_layers, num_classes)
+    
+    # Sample input
+    x = torch.rand((8, num_patches, patch_vec_size))
+    
+    output, att = model(x)
+    print(output.shape)
